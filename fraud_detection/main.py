48d2d0b50b771eade833fee4aa3f3ec29fb84d79
Oluwanifesimi Olubunmi <nifesimiolubunmi@gmail.com>
2025-06-17 12:07:38 +0100

    downloaded and extracted dataset

--- fraud_detection/main.py
+++ fraud_detection/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from utilities.kaggle_downloader import download_and_extract
 
+download_and_extract("mlg-ulb/creditcardfraud", output_folder="data")
 #load dataset
-df = pd.read_csv("creditcard.csv")
+df = pd.read_csv("data/creditcard.csv")
 print(df.shape)
